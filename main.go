14b2a60858cc5af142afcb263960fdf48859f9fb
andyrees <andyrees3@gmail.com>
2015-03-23 21:35:06 +0000

    updated

--- main.go
+++ main.go
@@ -13,8 +13,11 @@ func main() {
 		Title:       "andyrees.github.io blog",
 		Link:        &feeds.Link{Href: "http://andyrees.github.io/"},
 		Description: "Discussion about programming in go and transcoding",
-		Author:      &feeds.Author{"Andy Rees", "https://twitter.com/andygolang"},
-		Created:     now,
+		Author: &feeds.Author{
+			Name:  "Andy Rees",
+			Email: "https://twitter.com/andygolang",
+		},
+		Created: now,
 	}
 
 	feed.Items = []*feeds.Item{
@@ -22,8 +25,11 @@ func main() {
 			Title:       "Francesc Campoy Flores blog",
 			Link:        &feeds.Link{Href: "http://www.campoy.cat/"},
 			Description: "Developer Advocate at Google",
-			Author:      &feeds.Author{"Francesc Campoy Flores", "user@name.net"},
-			Created:     now,
+			Author: &feeds.Author{
+				Name:  "Francesc Campoy Flores",
+				Email: "user@name.net",
+			},
+			Created: now,
 		},
 		{
 			Title:       "Official Go blog",
